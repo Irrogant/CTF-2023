e98f481efc28871eb7b1dbc1703e0a857ca71959
Irrogant <irenmichelle@gmail.com>
2023-09-01 14:03:35 +0300

    Small changes

--- app.py
+++ app.py
@@ -8,24 +8,26 @@ def index():
     content = render_template("frontend.html")
     response = make_response(content)
 
+    # Check which type of request it is:
+    
     # Check value of cookie
     visitor = request.cookies.get("visitor")
 
-    if visitor == "wizard":
+    if visitor and visitor.lower() == "wizard":
         response.headers["Ancient-Rat-Rambles"] = (
         "With your wizardly aura, the rat's gaze shifts, "
         "and it releases its grip on the bars. Its whiskers twitch, " 
         "eyes locking onto yours, gleaming with recognition and power. "
         "A realization dawns within you: "
-        "the truth was always omnipresent, a spark within. "
-        "Having fulfilled its purpose, the rat returns to its vigil, "
-        "paws embracing the bars once more. It rattles them, "
-        "as if to erase the revelation, as if nothing had occurred. "
-        "But as a true wizard, you have unraveled the guarded enigma: flag {dddddd}."
+        "the truth was always present, a spark, a bitter taste, a headache."
+        "Having fulfilled its purpose, the rat returns to its vigil state, "
+        "paws embracing the bars once more, as if nothing had just occured. "
+        "But as a true wizard of enchanted elixirs, "
+        "you have unraveled one of many truths: date_flag{D0nk3r0Sha11N0tB3Shugg3d}."
         )
         return response
     
-    if visitor == "rat":
+    if visitor  and visitor.lower() == "rat":
         response.headers["Ancient-Rat-Rambles"] = (
         "The rat's eyes widen like twin moons, beholding a humongous being among its kind. "
         "It emits a triumphant sequence: 'chirp chirp chorp squeee.' The ancient echoes "
@@ -34,13 +36,16 @@ def index():
         )
         return response
     
+    else:
+         response.headers["Ancient-Rat-Rambles"] = (
+        "The rat gazes at you with wide eyes, its delicate paws clutched around the imprisoning bars.  "
+        "Its teeth, worn and resolute, seem to resonate with the unspoken knowledge. Yet, it hesitates, "
+        "aware that the secrets it guards are only meant for those who possess the essence of a true wizard."
+        )
+
     # Set cookie to normal value
     response.set_cookie("visitor", value="human")
 
-    response.headers["Ancient-Rat-Rambles"] = (
-    "The rat gazes at you with its wide eyes, its delicate paws clutched around the imprisoning bars.  "
-    "Its teeth, worn and resolute, seem to resonate with the unspoken knowledge. Yet, it hesitates, "
-    "aware that the secrets it guards are only meant for those who possess the essence of a true wizard."
-    )
+   
 
     return response
